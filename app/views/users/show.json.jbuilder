5b934a01090f98508108251a20f8e99247e4b777
Eeshwar G <eeshwarg13@gmail.com>
2016-10-03 11:17:39 +0530

    Create new scaffold 'User'

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :fName, :lName, :username, :password, :gender, :dob, :age, :budget, :email, :favClub, :created_at, :updated_at
---
+++ db/migrate/20161003054622_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :fName
+      t.string :lName
+      t.string :username
+      t.string :password
+      t.string :gender
+      t.date :dob
+      t.integer :age
+      t.decimal :budget
+      t.string :email
+      t.string :favClub
+
+      t.timestamps
+    end
+  end
+end
