8ceb27a78dd77710e96de6b2492536e64d10385e
Eeshwar G <eeshwarg13@gmail.com>
2016-10-03 11:12:33 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FantasyPremierLeague::Application.config.secret_key_base = '38ae5763b3e81ed80c6507be8b5d750ffd4fdd107368ecbccf2790a091d462371eb9635b201d0e3914453705c0d4e9670f3c6bdf2c902821fc17dd8085b42bdf'
